8d91e6d8c12fa242fd7595f05e6afdb160151198
yllopes <ygorlopesm@hotmail.com>
2024-10-09 12:30:31 -0300

    FEAT:Adicão CRUD da Classe Role

--- BackEnd/App/Model/Role.php
+++ BackEnd/App/Model/Role.php
@@ -7,8 +7,7 @@ class Role {
     private $id;
     private $name;
 
-    public function __construct($id, $name) {
-        $this->id = $id;
+    public function __construct($name) {
         $this->name = $name;
 
     }
@@ -21,11 +20,7 @@ public function getName(){
     return $this->name;
 }
 
-public function setId(){
-    $this->id = $id;
-}
-
-public function setName(){
+public function setName($name){
     $this->name = $name;
 }
 
---
+++ BackEnd/Database/Connection.php
@@ -0,0 +1,31 @@
+<?php
+namespace Pi\Visgo\Database;
+
+use PDO;
+use PDOException;
+
+class Connection {
+    
+    private static $instance = null;
+    private $connection;
+    private $dns = 'mysql:host=localhost;dbname=nome_do_banco';
+    private $usuario = 'root';
+    private $senha = 'root';
+
+
+    private function __construct() {
+        try {
+            $this->connection = new PDO($this->dns, $this->usuario, $this->senha);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Falha na conexão: ' . $e->getMessage();        
+        } 
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance->connection;
+    }
+}
