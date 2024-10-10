8ad60a38707ae966562cbe440848391c1772c822
fulano <fulaninho@gmail.com>
2024-10-10 18:03:08 -0300

    reoganizaçao dos paths do html

---
+++ FrontEnd/dev/criar_usuario.6a79be30.js
@@ -0,0 +1,12 @@
+// cria a tamplate string
+const headerHtml = `
+        <img src="docs/visgo.jpg" alt="logo">
+    `;
+// cria o header no hmtl 
+const headerElement = document.createElement("header");
+// header no html recebe o conteudo da template string
+headerElement.innerHTML = headerHtml;
+// aplica o elemento header ao body html
+document.body.insertAdjacentElement("beforeend", headerElement);
+
+//# sourceMappingURL=criar_usuario.6a79be30.js.map
