b17b5ab8f25b9c97855dc25ae9a60990a4653b47
kani-a <allanchagasilva@gmail.com>
2024-10-13 15:22:07 -0300

    adicionado botao de voltar ao header

--- FrontEnd/assets/js/header.js
+++ FrontEnd/assets/js/header.js
@@ -4,9 +4,8 @@ const headerHtml = `
         <section  class=" w-100 p-2 text-center  d-flex justify-content-between">
         
         <img src="assets/imgs/visgo.jpg" alt="logo" class="img-fluid">
-    <a href="tela_login.html"><button class="btn btn-success  align-self-start ">  voltar </button></a>
-        </section>
-    
+        <button id="voltar-btn"></button>
+  
 `;
 // tem q fazer o botao voltar pra pagina anterior nao para tela login
 
@@ -18,3 +17,14 @@ const headerHtml = `
     headerElement.innerHTML = headerHtml;
 // aplica o elemento header ao body html
     document.body.insertAdjacentElement('afterbegin', headerElement)
+
+    
+    const voltarButton = document.createElement('button');
+    voltarButton.innerHTML = 'Voltar';
+    voltarButton.id = 'voltar-btn'; // ID específico para o novo botão
+    // Adicionando o evento de clique
+    voltarButton.addEventListener('click', () => {
+        window.history.go(-1); // Altere para o caminho correto
+    });
+    // Adicionando o botão ao body ou a um contêiner específico
+    document.body.appendChild(voltarButton);
--- FrontEnd/assets/js/paginas/criar_usuarios.js
+++ FrontEnd/assets/js/paginas/criar_usuarios.js
@@ -76,7 +76,7 @@ let criar_user_html = `
 
 
 
-
 let criar_user_element = document.createElement("main");
 criar_user_element.innerHTML = criar_user_html;
 document.body.appendChild(criar_user_element)
+
