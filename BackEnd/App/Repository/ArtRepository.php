1534e4038901aa262fdf0698fbf28b2dda660b36
yllopes <ygorlopesm@hotmail.com>
2024-10-14 12:25:09 -0300

    FEAT: inicializacao do da class art

---
+++ BackEnd/App/Repository/ArtRepository.php
@@ -0,0 +1,88 @@
+<?php
+namespace Pi\Visgo\Repository;
+
+use Pi\Visgo\Model\Art;
+use Pi\Visgo\Database\Connection;
+use PDO;
+
+class ArtRepository {
+    private $connection;
+    private $table = "art";
+
+
+    public function __construct($drive){
+        $this->connection = Connection::getInstance($drive);
+    }
+
+    public function createrArt(Art $art){
+        $name = $art->getName();
+        $description = $art->getDescription();
+        $characteristic = $art->getCharacteristic();
+
+
+        $query = "INSERT INTO $this->table (name, description, characteristic) VALUES (:name, :description, :characteristic)";
+
+        $stmt = $this->connection->prepare($query);
+        
+        $stmt->bindParam(":name, $name");
+        $stmt->bindParam(":description, $description");
+        $stmt->bindParam(":characteristic, $characteristic");
+
+        $executionCompleted = $stmt->execute();
+
+        return $executionCompleted;
+
+}
+
+    public function updateArt($id, Art $art){
+        $name = $art->getName();
+        $description = $art->getDescription();
+        $characteristic = $art->getCharacteristic();
+        
+        
+        $query = "UPDATE $this->table SET name = :name, description = :description, characteristic = :characteristic WHERE art.id = :art_id";
+
+        $stmt = $this->connection->prepare($query);
+
+        $stmt->bindParam(":name, $name");
+        $stmt->bindParam(":description, $description");
+        $stmt->bindParam(":characteristic, $characteristic");
+        $stmt->bindParam(":art_id, $id");
+
+        $executionCompleted = $stmt->execute();
+
+        return $executionCompleted;
+
+}
+
+    public function searchByIdArt($id){
+        $query = "SELECT * FROM $this->table WHERE art.id = :id";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        return $stmt->fecth(PDO::FETCH_ASSOC);
+    }
+
+    public function getAllArt(){
+        $query = "SELECT * FROM $this->table";
+        $stmt = $this->connection->prepare($query);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function deleteByIdArt($id){
+        $arraySearch = $this->searchByIdArt($id);
+        $query = "DELETE FROM ART WHERE art.id = :id";
+        $stmt = $this->connection->prepare($query);
+
+        $stmt->bindParam(":id", $id);
+
+        $executionCompleted = $stmt->execute();
+
+        return $executionCompleted;
+        
+    }
+
+}
