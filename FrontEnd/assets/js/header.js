d1a37c4d99b58844f83ba85397d5a42a956a8071
kani-a <allanchagasilva@gmail.com>
2024-10-15 17:18:33 -0300

    coreçao de botao q esta fora do header

--- FrontEnd/assets/js/header.js
+++ FrontEnd/assets/js/header.js
@@ -1,30 +1,31 @@
 
 // Cria a template string para o header
 const headerHtml = `
-        <section  class=" w-100 p-2 text-center  d-flex justify-content-between">
-        
-        <img src="assets/imgs/visgo.jpg" alt="logo" class="img-fluid">
-        <button id="voltar-btn"></button>
-  
+            <section  class=" w-100 p-2 text-center  d-flex justify-content-between">
+            
+            <img src="assets/imgs/visgo.jpg" alt="logo" class="img-fluid">
+         <button id="voltar_btn" class="btn btn-success h-25" >voltar</button>
+  </section>
 `;
-// tem q fazer o botao voltar pra pagina anterior nao para tela login
-
-// Insere o header no início do body
 
 // cria o header no hmtl 
     const headerElement = document.createElement('header');
 // header no html recebe o conteudo da template string
     headerElement.innerHTML = headerHtml;
 // aplica o elemento header ao body html
-    document.body.insertAdjacentElement('afterbegin', headerElement)
+    document.body.insertAdjacentElement('afterbegin', headerElement);
 
     
-    const voltarButton = document.createElement('button');
-    voltarButton.innerHTML = 'Voltar';
-    voltarButton.id = 'voltar-btn'; // ID específico para o novo botão
-    // Adicionando o evento de clique
+  
+
+  let voltarButton = document.querySelector("#voltar_btn");
     voltarButton.addEventListener('click', () => {
-        window.history.go(-1); // Altere para o caminho correto
+        console.log("teste");
+        
+         window.history.go(-1);
     });
     // Adicionando o botão ao body ou a um contêiner específico
-    document.body.appendChild(voltarButton);
+    document.body.appendChild(headerElement);
+
+// Insere o header no início do body
+
