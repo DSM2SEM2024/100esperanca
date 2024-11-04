3f0cdb4f48e144f56856899df8569c5aedb84c5d
kani-a <allanchagasilva@gmail.com>
2024-11-04 20:18:47 -0300

    CHORE - home adicionado ao index.js

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,5 +1,6 @@
 import { headerHtml } from "./components/header";
 import { clearBody } from "./functions/clear_body";
+import { criaHomeHTML } from "./pages/home/home";
 import { telaLoginHtml } from "./pages/login_screen";
 import { criarUsuarioHtml } from "./pages/create_user";
 import { gerenciarUsuarioHtml } from "./pages/user_management";
@@ -16,9 +17,9 @@ if (location.hash === '#login') {
 }else if(location.hash === '#gerenciarUsuario'){
     clearBody();
     gerenciarUsuarioHtml();
-}else if(location.hash === '#home'){
-    clearBody();
-   
+}else if(!location.hash || location.hash === '#home'){
+    clearBody();  
+    criaHomeHTML();
 }
 };
 
