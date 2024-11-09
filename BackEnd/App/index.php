6f3eb20ea8ca698138a6fe87fc399c90bcb6d640
Ygor Lopes <164409074+yllopes@users.noreply.github.com>
2024-11-09 10:59:14 -0300

    Update index.php

--- BackEnd/App/index.php
+++ BackEnd/App/index.php
@@ -18,49 +18,20 @@ $uri = $_SERVER['REQUEST_URI'];
 
 switch ($method) {
     case 'GET':
-        if(preg_match('/\/order\/(\d+)/', $uri, $match)){
-            $id = $match[1];
-            $data = json_decode(file_get_contents('php://input'));
-             $orderController->searchById($id);
-        }
-            if ($uri === '/order_art'){
-                $orderController->getAllOrderArt();
-        }
-        
+ 
     break;
 
     case 'POST':
-        if($uri === '/order_art'){
-            $data = json_decode(file_get_contents('php://input'));
-            $orderController->addArtToOrder($orderId,$arts);
-        }
+
 
     break;
 
     case 'PUT':
-        // if(preg_match('/\/order\/(\d+)/', $uri, $match)){
-        //     $id = $match[1];
-        //     $data = json_decode(file_get_contents('php://input'));
-        //     $orderController->finishOrder($id);
-        // }
-        
-        if(preg_match('/\/order\/(\d+)/', $uri, $match)){
-            $id = $match[1];
-            $data = json_decode(file_get_contents('php://input'));
-            $orderController->reopenOrder($id);
-        }
-
-        
         
     break;
 
     case 'DELETE':
 
-        $data = json_decode(file_get_contents('php://input'));
-    $orderController->removeArtFromOrder($orderId, $artId);
-
-       
-        
-        break;
+    break;
 }
 
