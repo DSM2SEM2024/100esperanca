108f22aafc6b7ab60c53d4bab46cdf91cd4a725a
kani-a <allanchagasilva@gmail.com>
2024-11-11 16:13:38 -0300

    CHORE - navbar pagina gerenciar usuario refatorada

--- FrontEnd/assets/js/pages/user_management.js
+++ FrontEnd/assets/js/pages/user_management.js
@@ -4,13 +4,30 @@ const main = getOrCreateMainElement()
 main.classList = 'vh-100 d-block';
 export function gerenciarUsuarioHtml() {
   const gerenciarUsuario = `
-      <div>
-        <ul>
-          <li> texto1 </li>
-          <li> texto2 </li>
-          <li> texto3  </li>
-        </ul>
-      <div>
+      <section class="side-navbar">
+
+
+           
+    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Enable body scrolling</button>
+    
+    <div class="offcanvas offcanvas-start hide" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel" aria-modal="true" role="dialog">
+      <div class="offcanvas-header m-4">
+        <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body p-5">
+      <p>Try scrolling the rest of the page to see this option in action.</p>
+      <p>Try scrolling the rest of the page to see this option in action.</p>
+      <p>Try scrolling the rest of the page to see this option in action.</p>
+      <p>Try scrolling the rest of the page to see this option in action.</p>
+      </div>
+    </div>
+    
+
+
+
+
+      </section>
 
 
         <section class="container d-block p-2 align-middle justify-content-center">
