bfdf6e489267fd93bccbfedb61f66631b3f00f96
kani-a <allanchagasilva@gmail.com>
2024-11-30 19:15:12 -0300

    FEAT - RE-ADEQUAÇAO DOS PARAGRAPOS

--- FrontEnd/assets/js/pages/home/home.js
+++ FrontEnd/assets/js/pages/home/home.js
@@ -18,10 +18,10 @@ export function criaHomeHTML(){
     </section>
   
 
-    <section class="countainer-xxl d-flex p-4 flex-wrap">
-        <section class="countainer-sm d-inline" id="first-container">
-            <h1>inicio de tudo</h1>
-            <p class="fs-5 text-wrap">
+    <section class="countainer-xxl d-flex flex-wrap">
+        <section class="countainer-sm d-inline" id="first-container py-5">
+            <h1 class="h1 text-center">inicio de tudo</h1>
+            <p class="px-3 fs-4 text-wrap">
                 adiburai naxumerus silimulambukaidro Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa similique iste fugit hic atque blanditiis praesentium, esse quidem accusantium perferendis qui at ad voluptatibus quia saepe veritatis voluptas illo! Culpa repudiandae vero totam, illum at consectetur nihil doloremque voluptates ullam laboriosam tempora ad id cumque ab labore. Accusamus harum voluptatibus, delectus id nemo illo dicta consequatur ipsa nulla recusandae quibusdam et! Quae eos corporis reiciendis quibusdam debitis sed mollitia tenetur et porro ab, modi vel ipsum eveniet neque vitae perspiciatis! Cupiditate illo mollitia necessitatibus porro sit alias voluptatem distinctio deserunt dolor rerum? Expedita laudantium ex, accusantium delectus sed officia totam? 
             </p>
         </section>
@@ -31,32 +31,32 @@ export function criaHomeHTML(){
 
     <section class="d-flex flex-row" id="countent">
         <section class="countainer-sm d-inline text-center text-wrap" id="our_objective">
-            <h1>
+            <h1  class="h1 text-center">
                 <i class="bi bi-flag-fill"></i>
                 Qual nosso objetivo?
             </h1>
-            <p class="p-4">
+            <p class="px-3 fs-4">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni molestiae tempora voluptate dolore, itaque maxime? Veniam labore, magni error fuga ipsa quod iusto porro aperiam voluptates. Inventore maxime blanditiis cumque animi ratione perferendis consectetur error quisquam et perspiciatis sapiente at officia, praesentium incidunt? Ex aperiam dolores distinctio, tempore commodi laboriosam?
             </p>
 
         </section>
-        <section class="countainer-sm d-inline text-center text-wrap" id="nao_sei_oq_colocar_aqui">
-            <h1>
+        <section class="  countainer-sm d-inline text-center text-wrap" id="nao_sei_oq_colocar_aqui">
+            <h1  class="h1 text-center">
                 <i class="bi bi-code-slash"></i>
                  Desenvolvimento do site
             </h1>
-            <p class="p-4">
+            <p class="px-3 fs-4">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni molestiae tempora voluptate dolore, itaque maxime? Veniam labore, magni error fuga ipsa quod iusto porro aperiam voluptates. Inventore maxime blanditiis cumque animi ratione perferendis consectetur error quisquam et perspiciatis sapiente at officia, praesentium incidunt? Ex aperiam dolores distinctio, tempore commodi laboriosam?
             </p>
             
         </section>
     </section>
     <hr>
-        <section class="countainer-sm d-flex flex-column text-center align-items-center text-wrap" id="nao_sei_oq_colocar_aqui">
+        <section class=" countainer-sm d-flex flex-column text-center align-items-center text-wrap" id="nao_sei_oq_colocar_aqui">
             <img src="${img}" alt="visgo main logo" class="img-thumbnail" style="border-radius: 100%;" width="400rem">
 
-            <h1>VISGO DE JACA</h1>
-            <p class="p-4 fs-4">
+            <h1  class="h1 text-center">VISGO DE JACA</h1>
+            <p class="px-3 fs-4">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni molestiae tempora voluptate dolore, itaque maxime? Veniam labore, magni error fuga ipsa quod iusto porro aperiam voluptates. Inventore maxime blanditiis cumque animi ratione perferendis consectetur error quisquam et perspiciatis sapiente at officia, praesentium incidunt? Ex aperiam dolores distinctio, tempore commodi laboriosam?
             </p>
             
