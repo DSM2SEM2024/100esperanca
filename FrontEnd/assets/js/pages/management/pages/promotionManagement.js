ab89e5660d48b33d58ad5b8f70fefa5bbbc122fd
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2024-12-02 10:20:05 -0300

    REFACTOR: recuperação da nova tela de gerenciamento de promotions, perdida em aceite do PR

--- FrontEnd/assets/js/pages/management/pages/promotionManagement.js
+++ FrontEnd/assets/js/pages/management/pages/promotionManagement.js
@@ -1,81 +1,204 @@
 import { getOrCreateMainElement } from "../../../components/main";
+import { baseUrl } from "../../../services/baseUrl/baseUrl";
 import { sidebar } from "../components/sidebar";
-
 const main = getOrCreateMainElement();
 
 export function telaGerenciarPromocoes() {
   const gerenciarPromocoes = `
-    <div class="container mt-4">
-      <h2 class="text-center mb-4">Gerenciar Promoções</h2>
-      <!-- Row para organizar os forms -->
-      <div class="row">
-        <!-- Coluna 1: Formulário para adicionar promoção -->
-        <div class="col-md-6 pb-3">
-          <form id="formAdicionarPromocao" class="border p-4 rounded shadow">
-            <h4 class="mb-3">Adicionar Promoção</h4>
-            <div class="mb-3">
-              <label for="nomePromocao" class="form-label">Nome da Promoção</label>
-              <input type="text" class="form-control" id="nomePromocao" placeholder="Digite o nome da promoção" required>
-            </div>
-            <div class="mb-3">
-              <label for="descontoPromocao" class="form-label">Desconto (%)</label>
-              <input type="number" class="form-control" id="descontoPromocao" placeholder="Digite o percentual de desconto" required>
-            </div>
-            <div class="mb-3">
-              <label for="selecionarProdutos" class="form-label">Selecionar Produtos</label>
-              <select multiple class="form-select" id="selecionarProdutos" required>
-                <option value="1">Produto 1 - ID: 001</option>
-                <option value="2">Produto 2 - ID: 002</option>
-              </select>
-            </div>
-          <div class="d-grid gap-2">
-                <button type="submit" class="btn btn-success">Adicionar Promoção</button>
-              </div>
-            </form>
-          </div>
-
-          <!-- Coluna 2: Formulário para gerenciar produtos em promoções -->
-          <div class="col-md-6">
-            <form id="formGerenciarProdutos" class="border p-4 rounded shadow">
-              <h4 class="mb-3">Gerenciar Produtos em Promoções</h4>
-              <div class="mb-3">
-                <label for="selecionarPromocao" class="form-label">Selecionar Promoção</label>
-                <select class="form-select" id="selecionarPromocao" required>
-                  <option value="promo1">Promoção 1</option>
-                  <option value="promo2">Promoção 2</option>
-                </select>
-              </div>
-
-              <div class="mb-3">
-                <label for="adicionarProduto" class="form-label">
-                  Adicionar Produto
-                </label>
-                  <input type="text" class="form-control mb-2" id="nomeProduto" placeholder="Nome do Produto">
-                  <div class="input-group mb-2 w-75">
-                    <input type="text" class="form-control" id="idProduto" placeholder="ID do Produto">
-                      <button class="btn bg-success">
-                        <span class="input-group-text bg-success border-0">
-                          <i class="bi bi-search text-white">
-                          </i>
-                        </span>
-                      </button>
-                  </div>
-              </div>
-
-              <div class="d-grid gap-2 justify-content-center">
-                <button type="button" class="btn btn-success">
-                  Adicionar Produto
-                </button>
-                <button type="button" class="btn btn-danger">
-                  Remover Produto
-                </button>
-              </div>
-            </form>
-          </div>
+    <section class="container-fluid p-4">
+      <h4 class="mb-3 fs-1 text-center">Gerenciar Promoções</h4>
+      <div class="mb-4">
+        <label for="pesquisarPromocao" class="form-label">Pesquisar Promoção</label>
+        <div class="input-group">
+          <input type="text" class="form-control" id="pesquisarPromocao" placeholder="Pesquisar por Código da Promoção ou Categoria">
+          <button class="btn btn-success rounded-end" type="button" id="botaoPesquisar">
+            <i class="bi bi-search text-white"></i>
+          </button>
+        </div>
+        <small class="text-muted">Pesquise por Código ou Data da Promoção</small>
       </div>
-    </div>
+      <div class="text-center">
+        <h2>Consulta de Promoções</h2>
+        <table class="table table-bordered table-responsive table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Código</th>
+              <th>Data de Início</th>
+              <th>Data de Encerramento</th>
+              <th>Categoria</th>
+            </tr>
+          </thead>
+          <tbody id="tabelaPromocoes">
+            <tr>
+              <td colspan="5">Nenhuma Promoção encontrada</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="d-flex justify-content-center">
+        <button type="button" class="btn btn-success" id="abrirModalAdicionarPromocao">
+          Adicionar Promoção
+        </button>
+      </div>
+    </section>
+    ${modalAddPromocao()}
   `;
-  main.innerHTML = gerenciarPromocoes;
 
-  sidebar()
+  function modalAddPromocao() {
+    return `
+      <div class="modal fade" id="modalAddPromocao" tabindex="-1" aria-labelledby="modalAdicionarPromocaoLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title text-success" id="modalAdicionarPromocaoLabel">Adicionar Promoção</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form id="formAdicionarPromocao" class="border p-4 rounded shadow">
+                <h4 class="mb-3">Adicionar Promoção</h4>
+                <div class="mb-3">
+                  <label for="codPromocao" class="form-label">Código da Promoção</label>
+                  <input type="text" class="form-control" id="codPromocao" placeholder="Digite o código da promoção" required>
+                </div>
+                <div class="mb-3">
+                  <label for="startDatePromocao" class="form-label">Data de Início</label>
+                  <input type="datetime-local" class="form-control" id="startDatePromocao" required>
+                </div>
+                <div class="mb-3">
+                  <label for="endDatePromocao" class="form-label">Data de Término</label>
+                  <input type="datetime-local" class="form-control" id="endDatePromocao" required>
+                </div>
+                <div class="d-grid gap-2">
+                  <button type="submit" class="btn btn-success">Adicionar Promoção</button>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+              <button type="button" class="btn btn-success" id="btnSalvarPromocao">Salvar Promoção</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  main.innerHTML = gerenciarPromocoes;
+  sidebar();
+
+  // Event listener para abrir o modal
+  const abrirModalButton = document.getElementById(
+    "abrirModalAdicionarPromocao"
+  );
+  abrirModalButton.addEventListener("click", () => {
+    const modal = new bootstrap.Modal(
+      document.getElementById("modalAddPromocao")
+    );
+    modal.show();
+  });
+
+  // Event listener para salvar a promoção
+  document
+    .getElementById("btnSalvarPromocao")
+    .addEventListener("click", async () => {
+      const form = document.getElementById("formAdicionarPromocao");
+      const formData = new FormData(form);
+
+      const novaPromocao = {
+        start_date_promotion: formData.get("startDatePromocao"),
+        end_date_promotion: formData.get("endDatePromocao"),
+        cod_promotion: formData.get("codPromocao"),
+      };
+
+      try {
+        const response = await fetch(`${baseUrl}promotions`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(novaPromocao),
+        });
+
+        if (response.ok) {
+          alert("Promoção criada com sucesso!");
+          form.reset();
+          const modal = bootstrap.Modal.getInstance(
+            document.getElementById("modalAddPromocao")
+          );
+          modal.hide();
+          renderTabelaPromocoes(); // Atualiza a tabela de promoções
+        } else {
+          const errorData = await response.json();
+          alert(`Erro ao criar promoção: ${errorData.message}`);
+        }
+      } catch (error) {
+        console.error("Erro ao criar promoção:", error);
+        alert("Erro ao criar promoção. Tente novamente.");
+      }
+    });
+
+  document
+    .getElementById("botaoPesquisar")
+    .addEventListener("click", async () => {
+      const inputPesquisar = document.getElementById("pesquisarPromocao").value;
+      if (!inputPesquisar) {
+        renderTabelaPromocoes(); // Se não houver pesquisa, exibe todas as promoções
+        return;
+      }
+
+      try {
+        const response = await fetch(
+          `${baseUrl}promotions/${inputPesquisar}`
+        );
+        if (response.ok) {
+          const promocao = await response.json();
+          renderTabelaPromocoes([promocao]);
+        } else {
+          const errorData = await response.json();
+          alert(`Erro ao buscar promoção: ${errorData.message}`);
+        }
+      } catch (error) {
+        console.error("Erro ao buscar promoção:", error);
+        alert("Erro ao buscar promoção. Tente novamente.");
+      }
+    });
+}
+
+async function renderTabelaPromocoes(promocoes = null) {
+  const tabela = document.getElementById("tabelaPromocoes");
+  tabela.innerHTML = "<tr><td colspan='5'>Carregando...</td></tr>";
+
+  try {
+    const promocoesLista = promocoes || (await getAllPromotions());
+    if (!promocoesLista || promocoesLista.length === 0) {
+      tabela.innerHTML =
+        "<tr><td colspan='5'>Nenhuma promoção encontrada</td></tr>";
+      return;
+    }
+
+    tabela.innerHTML = promocoesLista
+      .map(
+        (promocao) => `
+          <tr data-id="${promocao.id}">
+            <td>${promocao.id}</td>
+            <td>${promocao.cod_promotion}</td>
+            <td>${new Date(promocao.start_date_promotion).toLocaleString()}</td>
+            <td>${new Date(promocao.end_date_promotion).toLocaleString()}</td>
+          </tr>
+        `
+      )
+      .join("");
+  } catch (error) {
+    tabela.innerHTML = `<tr><td colspan="5">Erro ao carregar as promoções: ${error.message}</td></tr>`;
+  }
+}
+
+async function getAllPromotions() {
+  const response = await fetch(`${baseUrl}promotions`);
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error("Promoção Não Encontrada");
+  }
 }
