c1c2662bc8eeb4b3c6ecdb941f6a94c9a9eb9225
kani-a <allanchagasilva@gmail.com>
2024-12-31 21:08:23 -0300

    commit de ano novo, eu nao lembro oq eu fiz

--- FrontEnd/assets/js/components/header.js
+++ FrontEnd/assets/js/components/header.js
@@ -2,6 +2,7 @@ import visgo from "../../imgs/visgo.jpg";
 
 export const headerHtml = `
 <header class="w-100 p-2 text-center d-flex justify-content-between align-items-center">
+
   <section>
     <img src="${visgo}" alt="LogoVisgo" class="img-fluid">
     <button id="voltar_btn" class="btn ms-3 btn-sm btn-success">
@@ -9,21 +10,61 @@ export const headerHtml = `
     </button>
   </section>
 
-  <nav class="navbar  d-flex justify-content-evenly flex-wrap row">
-    <section class="d-flex flex-wrap justify-content-center col">
-        <a id="home-Link" href="#home" class="text-decoration-none text-success" role="button">
-          <button class="btn btn-outline-success border-0">Home</button>
-        </a>
-
+  <nav class="horizontal-nav d-flex">
         <a id="manageUser-Link" href="#gerenciarUsuarios" class="text-decoration-none text-success" role="button">
-          <button class="btn btn-outline-success border-0">Tela Admin</button>
+          <button class="btn btn-outline-success w-100 border-0">
+            Tela Admin
+          </button>
         </a>
 
         <a id="produtos-Link" href="#produtos" class="text-decoration-none text-success" role="button">
-          <button class="btn btn-outline-success border-0">Produtos</button>
+              <button class="btn btn-outline-success w-100 border-0">
+                 Produtos
+              </button>
+          </a>
+        <a id="produtos-Link" href="#produtos" class="text-decoration-none text-success" role="button">
+          <button class="btn btn-outline-success w-100 border-0">
+              Produtos
+          </button>
         </a>
-    </section>
   </nav>
+
+
+
+  <nav class="navbar d-flex justify-content-evenly p-0 m-0" id="mobile-nav">
+    <ul class="navbar-nav">
+      <li class="nav-item dropbottom">
+        <button class="btn btn-success btn-sm  dropdown-toggle-split rounded-3" data-bs-toggle="dropdown" aria-expanded="false">
+        <p class="fs-5 m-0">
+           menu
+        </p>
+        
+        </button>
+        <ul class="dropdown-menu dropdown-menu position-absolute">
+          <li>
+             <a id="manageUser-Link" href="#gerenciarUsuarios" class="text-decoration-none text-success" role="button">
+          <button class="btn btn-outline-success w-100 border-0">Tela Admin</button>
+        </a>
+          </li>
+
+          <li>
+           <a id="produtos-Link" href="#produtos" class="text-decoration-none text-success" role="button">
+              <button class="btn btn-outline-success w-100 border-0">
+                 Produtos
+              </button>
+           </a>
+          </li>
+          <li>
+            <a id="home-Link" href="#home" class="text-decoration-none" role="button">
+              <button class="btn btn-outline-success w-100 border-0">
+                Home
+              </button>
+            </a>
+        </li>
+    </ul>
+  </nav>
+
+  
 
   <nav class="navbar px-3 d-flex justify-content-evenly">
     <ul class="navbar-nav">
@@ -78,7 +119,6 @@ export function updateNavbarLinks() {
 
   voltarButton.style.display = location.hash === '#home' || location.hash === '' ? 'none' : 'inline';
 }
-
 window.addEventListener('hashchange', updateNavbarLinks);
 window.addEventListener("load", updateNavbarLinks);
 updateNavbarLinks();
